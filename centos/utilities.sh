8b4803c674935f2d77d58ce9a682773e5e0e5197
Liam Bindle <liambindle@gmail.com>
2019-10-22 16:35:27 -0600

    fixes

--- centos/utilities.sh
+++ centos/utilities.sh
@@ -1,7 +1,13 @@
 set -x
 set -e
 
-yum install -y bzip2 zip wget curl autoconf automake flex bison make python git vim which environment-modules patch libsigsegv libtool texinfo findutils xorg-x11-util-macros libpciaccess-devel numactl libxml2-devel gettext help2man libuuid-devel cmake3
+yum install -y bzip2 zip wget curl autoconf automake flex bison make python git vim which environment-modules patch libsigsegv libtool texinfo findutils xorg-x11-util-macros libpciaccess-devel numactl libxml2-devel gettext help2man libuuid-devel
+
+wget https://github.com/Kitware/CMake/releases/download/v3.14.7/cmake-3.14.7-Linux-x86_64.tar.gz
+tar -xf cmake-*
+cp -r cmake-*/ /opt/cmake
+rm -rf cmake-*
+echo "export PATH=$PATH:/opt/cmake/bin" >> /init.rc 
 
 mkdir /modulefiles
 mkdir /modulefiles/gcc
